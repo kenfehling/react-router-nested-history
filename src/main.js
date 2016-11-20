113e2151af1152ceb8856f1a3d1b169535f65187
Ken Fehling <ken@androidideas.org>
2016-11-20 17:09:27 -0500

    Added (naive) pop function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import * as behavior from './behaviors/defaultTabBehavior';
 import { getState, setState } from './historyStore';
+import * as _ from 'lodash';
 
-const _pushPage = (history, href) =>
-  ({back: [...history.back, history.current], current: href, forward: []});
+const _pushPage = (history, href) => ({
+  back: [...history.back, history.current],
+  current: href,
+  forward: []
+});
+
+const _popPage = (history) => ({
+  back: _.initial(history.back),
+  current: _.last(history.back),
+  forward: [history.current, ...history.forward]
+});
 
 const _updateTab = (state, tabIndex, fn) => [
   ...state.tabHistories.slice(0, tabIndex),
@@ -16,12 +26,22 @@ export function switchTab(fromIndex, toIndex) {
   setState({...state, currentTab: toIndex});
 }
 
-export function pushPage(href) {
+export function push(href) {
   const state = getState();
   const tabIndex = state.currentTab;
   setState({
     ...state,
     browserHistory: _pushPage(state.browserHistory, href),
     tabHistories: _updateTab(state, tabIndex, tab => _pushPage(tab, href))
+  });
+}
+
+export function pop() {
+  const state = getState();
+  const tabIndex = state.currentTab;
+  setState({
+    ...state,
+    browserHistory: _popPage(state.browserHistory),
+    tabHistories: _updateTab(state, tabIndex, _popPage)
   });
 }
--- test/main.test.js
+++ test/main.test.js
@@ -1,43 +1,10 @@
-import { switchTab, pushPage } from '../src/main';
+import { switchTab, push, pop } from '../src/main';
 import { getState, setState, clearState } from '../src/historyStore';
 
 describe('main', () => {
 
-  afterEach(() => {
-    clearState();
-  });
-
-  it('switches tab', () => {
+  beforeEach(() => {
     setState({
-      browserHistory: {back: [], current: '/a', forward: []},
-      currentTab: 0,
-      tabHistories: [
-        {back: [], current: '/a', forward: []},
-        {back: [], current: '/b', forward: []}
-      ]
-    });
-    switchTab(0, 1);
-    expect(getState()).toEqual({
-      browserHistory: {back: ['/a'], current: '/b', forward: []},
-      currentTab: 1,
-      tabHistories: [
-        {back: [], current: '/a', forward: []},
-        {back: [], current: '/b', forward: []}
-      ]
-    });
-  });
-
-  it('pushes page', () => {
-    setState({
-      browserHistory: {back: [], current: '/a', forward: []},
-      currentTab: 0,
-      tabHistories: [
-        {back: [], current: '/a', forward: []},
-        {back: [], current: '/b', forward: []}
-      ]
-    });
-    pushPage('/a/1');
-    expect(getState()).toEqual({
       browserHistory: {back: ['/a'], current: '/a/1', forward: []},
       currentTab: 0,
       tabHistories: [
@@ -47,4 +14,43 @@ describe('main', () => {
     });
   });
 
+  afterEach(() => {
+    clearState();
+  });
+
+  it('switches tab', () => {
+    switchTab(0, 1);
+    expect(getState()).toEqual({
+      browserHistory: {back: ['/a', '/a/1'], current: '/b', forward: []},
+      currentTab: 1,
+      tabHistories: [
+        {back: ['/a'], current: '/a/1', forward: []},
+        {back: [], current: '/b', forward: []}
+      ]
+    });
+  });
+
+  it('pushes page', () => {
+    push('/a/2');
+    expect(getState()).toEqual({
+      browserHistory: {back: ['/a', '/a/1'], current: '/a/2', forward: []},
+      currentTab: 0,
+      tabHistories: [
+        {back: ['/a', '/a/1'], current: '/a/2', forward: []},
+        {back: [], current: '/b', forward: []}
+      ]
+    });
+  });
+
+  it('pops page', () => {
+    pop();
+    expect(getState()).toEqual({
+      browserHistory: {back: [], current: '/a', forward: ['/a/1']},
+      currentTab: 0,
+      tabHistories: [
+        {back: [], current: '/a', forward: ['/a/1']},
+        {back: [], current: '/b', forward: []}
+      ]
+    });
+  });
 });
