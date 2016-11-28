9b934148a502fca8754f39550cc68fe441a8c074
Ken Fehling <ken@androidideas.org>
2016-11-28 12:37:21 -0500

    Need to pass in filter to get container order

--- whatever/src/index.js
+++ whatever/src/index.js
@@ -1,10 +1,10 @@
-import { setTabs, switchToTab, push } from '../../dist/react-nav-thing';
+import { setTabs, switchToTab, push } from '../../../dist/tab-history-library';
 import $ from 'jquery';
 
 $(() => {
-  setTabs('/a', '/b', '/c');
-  $('#tab1').click(() => switchToTab(0));
-  $('#tab2').click(() => switchToTab(1));
-  $('#tab3').click(() => switchToTab(2));
+  const tabs = setTabs('/a', '/b', '/c');
+  $('#tab1').click(() => tabs.switchToTab(0));
+  $('#tab2').click(() => tabs.switchToTab(1));
+  $('#tab3').click(() => tabs.switchToTab(2));
   $('#link1').click(() => push('/a/1'));
 });
