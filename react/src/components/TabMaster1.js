da75d93c16eb3f777866e6cf8e4fd9457b415632
Ken Fehling <ken@androidideas.org>
2016-11-28 09:45:30 -0500

    Example now pushes page

--- react/src/components/TabMaster1.js
+++ react/src/components/TabMaster1.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { push } from '../../../../dist/tab-history-library';
 
 export default () => (
     <div>
       <div>Tab 1</div>
-      <Link to="/tabs/1/balloon">Balloon</Link>
+      <a onClick={() => push("/tabs/1/balloon")}>Balloon</a>
     </div>
 );
