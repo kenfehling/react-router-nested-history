1705e5f1398460deb6401bbae11296ecc0f408c4
Ken Fehling <ken@androidideas.org>
2016-11-28 10:22:29 -0500

    Need to reorganize the data store

--- src/components/WindowMaster1.js
+++ src/components/WindowMaster1.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { push } from '../../../../dist/tab-history-library';
 
 export default () => (
   <div>
     <p>Hello</p>
-    <Link to="windows/1/boris">Special</Link>
+    <a onClick={() => push("/windows/1/boris")}>Boris</a>
   </div>
 );
--- src/components/WindowMaster2.js
+++ src/components/WindowMaster2.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HistoryLink } from '../../../dist/react-nav-thing';
 
 export default () => (
   <div>World</div>
