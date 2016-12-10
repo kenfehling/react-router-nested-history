75e7c822b22ad3237e17cdfc4c3b649686e58d3f
Ken Fehling <ken@androidideas.org>
2016-12-10 12:18:31 -0500

    Getting ready to preliminary release

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -1,20 +1,12 @@
 import React, { PropTypes, Component } from 'react';
 import { Tab as ReactTab, Tabs as ReactTabs, TabList, TabPanel } from 'react-tabs';
-import { setContainers, HistoryMatch } from '../../../../dist/tab-history-library';
+import { HistoryMatch, ContainerGroup, Container } from '../../../../dist/tab-history-library';
 import { TransitionMotion, spring } from 'react-motion';
 import './Tabs.css';
 import TabMaster1 from "../components/TabMaster1";
 import TabMaster2 from "../components/TabMaster2";
 import TabMaster3 from "../components/TabMaster3";
 import TabPage from "../components/TabPage";
-
-const tabsConfig = [
-  {initialUrl: '/tabs/1', urlPatterns: ['/tabs/1', '/tabs/1/*']},
-  {initialUrl: '/tabs/2', urlPatterns: ['/tabs/2', '/tabs/2/*']},
-  {initialUrl: '/tabs/3', urlPatterns: ['/tabs/3', '/tabs/3/*']}
-];
-
-const tabs = setContainers(tabsConfig);
 
 const MatchWithFade = ({ component:Component, ...rest }) => {
   const willEnter = () => ({ opacity: spring(1) });
@@ -50,84 +42,68 @@ const MatchWithFade = ({ component:Component, ...rest }) => {
 
 export const Tab1 = () => (
   <div>
-    <MatchWithFade exactly pattern="/tabs/1" component={TabMaster1} />
-    <MatchWithFade exactly pattern="/tabs/1/:page" component={TabPage} />
+    <Container initialUrl="/tabs/1" patterns={['/tabs/1', '/tabs/1/*']}>
+      <MatchWithFade exactly pattern="/tabs/1" component={TabMaster1} />
+      <MatchWithFade exactly pattern="/tabs/1/:page" component={TabPage} />
+    </Container>
   </div>
 );
 
 export const Tab2 = () => (
   <div>
-    <MatchWithFade exactly pattern="/tabs/2" component={TabMaster2} />
-    <MatchWithFade exactly pattern="/tabs/2/:page" component={TabPage} />
+    <Container initialUrl="/tabs/2" patterns={['/tabs/2', '/tabs/2/*']}>
+      <MatchWithFade exactly pattern="/tabs/2" component={TabMaster2} />
+      <MatchWithFade exactly pattern="/tabs/2/:page" component={TabPage} />
+    </Container>
   </div>
 );
 
 export const Tab3 = () => (
   <div>
-    <MatchWithFade exactly pattern="/tabs/3" component={TabMaster3} />
-    <MatchWithFade exactly pattern="/tabs/3/:page" component={TabPage} />
+    <Container initialUrl="/tabs/3" patterns={['/tabs/3', '/tabs/3/*']}>
+      <MatchWithFade exactly pattern="/tabs/3" component={TabMaster3} />
+      <MatchWithFade exactly pattern="/tabs/3/:page" component={TabPage} />
+    </Container>
   </div>
 );
-
-const onTabClick = (router, index) => {
-  /*
-  const containerPaths = getContainerPaths();
-  const containerPath = `/tabs/${index + 1}`;
-  if (isActiveTab(containerPath)) {
-    router.replaceWith(containerPath);  // Go to the top path in this container
-  }
-  else {
-  */
-    tabs.switchTo(index);
-  //}
-};
 
 class Tabs extends Component {
 
   constructor(props) {
     super(props);
+    const {containers} = props;
     this.state = {
-      currentTab: tabs.getActive()
+      currentTab: containers.getActive()
     };
   }
 
   componentWillMount() {
-    tabs.addChangeListener(state => {
+    const {containers} = this.props;
+    containers.addChangeListener(state => {
       this.setState({currentTab: state.activeContainer});
     })
   }
 
   render() {
-    const {router, match:matchContext} = this.props;
-    const onClick = onTabClick.bind(null, router);
+    const {containers} = this.props;
     return (<div>
       <h2>Tabs example</h2>
       <div className="description">
         <p>Each tab has its own individual history.</p>
         <p>Clicking on an already active tab goes to the top of its history stack.</p>
       </div>
-      <ReactTabs selectedIndex={this.state.currentTab.index}>
-        <TabList>
-          <ReactTab onClick={() => onClick(0)}>One</ReactTab>
-          <ReactTab onClick={() => onClick(1)}>Two</ReactTab>
-          <ReactTab onClick={() => onClick(2)}>Three</ReactTab>
-        </TabList>
-        <TabPanel>
-          <div className="tab-content">
-            <Tab1 />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="tab-content">
-            <Tab2 />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div className="tab-content">
-            <Tab3 />
-          </div>
-        </TabPanel>
-      </ReactTabs>
+      <ContainerGroup>
+        <ReactTabs selectedIndex={this.state.currentTab.index}>
+          <TabList>
+            <ReactTab onClick={() => containers.switchTo(0)}>One</ReactTab>
+            <ReactTab onClick={() => containers.switchTo(1)}>Two</ReactTab>
+            <ReactTab onClick={() => containers.switchTo(2)}>Three</ReactTab>
+          </TabList>
+          <TabPanel><div className="tab-content"><Tab1 /></div></TabPanel>
+          <TabPanel><div className="tab-content"><Tab2 /></div></TabPanel>
+          <TabPanel><div className="tab-content"><Tab3 /></div></TabPanel>
+        </ReactTabs>
+      </ContainerGroup>
     </div>);
   }
 }
@@ -137,4 +113,4 @@ Tabs.contextTypes = {
   match: PropTypes.any.isRequired
 };
 
-export default Tabs;
+export default createGroup(Tabs);
