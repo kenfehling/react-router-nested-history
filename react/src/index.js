09a8f3c575beb2cea6e12485d64dce2f28e7ccf6
Ken Fehling <ken@androidideas.org>
2017-01-02 12:03:25 -1000

    Redirect on /, etc.

--- react/src/index.js
+++ react/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Match, Miss, Redirect } from 'react-router'
 import { render } from 'react-dom'
 import App from './containers/App'
 import { HistoryRouter } from '../../../dist/react-router-nested-history'
 
 render((
   <HistoryRouter>
-    <App />
+    <div>
+      <Match pattern='/' exactly component={() => <Redirect to="/tabs/1" />} />
+      <Match pattern='/tabs' exactly component={() => <Redirect to="/tabs/1" />} />
+      <Match pattern='/windows' exactly component={() => <Redirect to="/windows/1" />} />
+      <Miss component={App} />>
+    </div>
   </HistoryRouter>
 ), document.getElementById('root'))
