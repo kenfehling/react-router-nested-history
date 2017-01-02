1edd51655cca7a89daf0859027b3d1e486846da0
Ken Fehling <ken@androidideas.org>
2017-01-02 13:26:11 -1000

    base name in examples

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -4,13 +4,16 @@ import { render } from 'react-dom'
 import App from './containers/App'
 import { HistoryRouter } from '../../../dist/react-router-nested-history'
 
+const getBasename = () => window.location.hostname === 'kenfehling.github.io' ?
+    'react-router-nested-history' : ''
+
 render((
-  <HistoryRouter>
+  <HistoryRouter basename={getBasename()}>
     <div>
       <Match pattern='/' exactly component={() => <Redirect to="/tabs/1" />} />
       <Match pattern='/tabs' exactly component={() => <Redirect to="/tabs/1" />} />
       <Match pattern='/windows' exactly component={() => <Redirect to="/windows/1" />} />
-      <Miss component={App} />>
+      <Miss component={App} />
     </div>
   </HistoryRouter>
 ), document.getElementById('root'))
