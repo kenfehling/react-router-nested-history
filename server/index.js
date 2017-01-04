a4917444d2c24c62f1e014f631b21c6305d6f83f
Ken Fehling <ken@androidideas.org>
2017-01-03 14:31:09 -1000

    Made getGroupState less strict

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+const root = path.resolve(__dirname, '..', 'build')
+
+// Serve static assets
+app.use(express.static(root))
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.join(root, 'index.html'))
+})
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
