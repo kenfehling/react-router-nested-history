2b86f79fb8efb9c7c70c6af21e4efc44f2f8422e
Ken Fehling <ken@androidideas.org>
2017-02-20 22:35:05 -0500

    Looking good so far

--- react/src/index.js
+++ react/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Match, Miss, Redirect } from 'react-router'
-import { render } from 'react-dom'
+import {Route, Redirect} from 'react-router'
+import {render} from 'react-dom'
 import App from './containers/App'
-import { HistoryRouter } from 'react-router-nested-history'
+import {HistoryRouter} from 'react-router-nested-history'
 
 render((
   <HistoryRouter>
     <div>
-      <Match pattern='/' exactly render={() => <Redirect to="/tabs/1" />} />
-      <Match pattern='/tabs' exactly render={() => <Redirect to="/tabs/1" />} />
-      <Match pattern='/windows' exactly render={() => <Redirect to="/windows/1" />} />
+      <Route path='/' exact render={() => <Redirect to="/tabs/1" />} />
+      <Route path='/tabs' exact render={() => <Redirect to="/tabs/1" />} />
+      <Route path='/windows' exact render={() => <Redirect to="/windows/1" />} />
       <App />
     </div>
   </HistoryRouter>
