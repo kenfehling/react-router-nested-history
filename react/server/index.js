01e2341cd571c6d4a0a5afe12fe44508449450e8
Ken Fehling <ken@androidideas.org>
2017-02-26 22:37:19 -0500

    Refactored server code

--- react/server/index.js
+++ react/server/index.js
@@ -1,36 +1,37 @@
 require('source-map-support').install()
-
 import express from 'express'
 import favicon from 'serve-favicon'
 import path from 'path'
 import compression from 'compression'
-import renderMiddleware from './middleware/render'
+import React from 'react'
+import App from '../src/components/App'
+import {renderToString} from 'react-dom/server'
+import {StaticRouter} from 'react-router'
 
 const app = express()
 app.use('/static', express.static(path.join(__dirname, 'build')))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-//app.set('views', path.join(__dirname, 'views'))
-//app.set('view engine', 'ejs')
-
-const isProduction = process.env.NODE_ENV === 'production'
-
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(compression())
 
-/*
-if (isProduction) {
-  app.use(compression())
-} else {
-  const {
-    webpackDevMiddleware,
-    webpackHotMiddleware,
-  } = require('./middleware/webpack')
+app.use((req, res) => {
+  const context = {};
 
-  app.use(webpackDevMiddleware)
-  app.use(webpackHotMiddleware)
-}
-*/
+  const html = renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>,
+  );
 
-app.use(renderMiddleware);
+  if (context.url) {
+    return res.redirect(302, context.url);
+  }
+
+  return res
+    .status(context.status || 200)
+    .render('index.ejs', {html})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Server running on port ${port}`));
