0b7e7d0b3e01e08e25733503524fcd74b5658fd8
Ken Fehling <ken@androidideas.org>
2017-02-26 22:58:38 -0500

    Fixed extra div, but library needs to be fixed to support SSR

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Route, Redirect} from 'react-router'
 import Tabs from './Tabs'
 import Windows from './Windows'
 import NestedGroup from './NestedGroup'
@@ -7,21 +8,27 @@ import StateTree from './StateTree'
 import './App.css'
 
 export default () => (
-  <div className="app-container">
-    <div className='left-container'>
-      <div className='top-container'>
-        <div className="tabs-container"><Tabs /></div>
-        <div className="windows-container"><Windows /></div>
-      </div>
-      <div className='bottom-container'>
-        <div className='nested-group-container'>
-          <NestedGroup />
+  <div>
+    <Route path='/' exact render={() => <Redirect to="/tabs/1" />} />
+    <Route path='/tabs' exact render={() => <Redirect to="/tabs/1" />} />
+    <Route path='/windows' exact render={() => <Redirect to="/windows/1" />} />
+    <Route path='/foods' exact render={() => <Redirect to="/foods/Fruit" />} />
+    <div className="app-container">
+      <div className='left-container'>
+        <div className='top-container'>
+          <div className="tabs-container"><Tabs /></div>
+          <div className="windows-container"><Windows /></div>
         </div>
-        <div className='scroll-area-container'>
-          <ScrollAreaExample />
+        <div className='bottom-container'>
+          <div className='nested-group-container'>
+            <NestedGroup />
+          </div>
+          <div className='scroll-area-container'>
+            <ScrollAreaExample />
+          </div>
         </div>
       </div>
+      <div className="state-tree-container"><StateTree /></div>
     </div>
-    <div className="state-tree-container"><StateTree /></div>
   </div>
 )
--- react/src/index.js
+++ react/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import {Route, Redirect} from 'react-router'
 import {render} from 'react-dom'
 import App from './components/App'
 import {HistoryRouter} from 'react-router-nested-history'
 
 render((
   <HistoryRouter>
-    <div>
-      <Route path='/' exact render={() => <Redirect to="/tabs/1" />} />
-      <Route path='/tabs' exact render={() => <Redirect to="/tabs/1" />} />
-      <Route path='/windows' exact render={() => <Redirect to="/windows/1" />} />
-      <Route path='/foods' exact render={() => <Redirect to="/foods/Fruit" />} />
-      <App />
-    </div>
+    <App />
   </HistoryRouter>
 ), document.getElementById('root'))
