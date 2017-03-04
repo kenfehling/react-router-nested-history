445835c3af55eb7921f82a23196f00fd56669f71
Ken Fehling <ken@androidideas.org>
2017-03-04 17:12:46 -0500

    Got a lot of mobile example working

--- src/components/MobileExample.js
+++ src/components/MobileExample.js
@@ -1,12 +1,31 @@
 import React, {PropTypes, Component } from 'react'
 import {
-  Container, WindowGroup, HistoryRoute, HistoryLink, HistoryWindow
+  Container, WindowGroup, HistoryRoute, HistoryLink, HistoryWindow, BackLink
 } from 'react-router-nested-history'
 import Helmet from 'react-helmet'
 import './MobileExample.css'
 
 const toId = name => name.toLowerCase()
 const toPath = name => `/mobile/${toId(name)}`
+
+const MobilePage = ({title, children}) => (
+  <div className='mobile-page'>
+    <div className='nav'>
+      <div className='back'>
+        <BackLink nameFn={({page='Home'}) => (
+          <div className='link'>
+            <i className="fa fa-chevron-left" />
+            <div className='text'>{page}</div>
+          </div>
+        )} />
+      </div>
+      <h1 className='title'>{title}</h1>
+    </div>
+    <div className='content'>
+      {children}
+    </div>
+  </div>
+)
 
 const MobileWindow = ({name, path=toPath(name), component, children, isDefault=false}) => (
   <HistoryWindow forName={toId(name)} className='mobile-window'>
@@ -51,22 +70,33 @@ const HomeScreen = ({onIconClick}) => (
 )
 
 export default () => (
-  <WindowGroup name='mobile' allowInterContainerHistory={true}>
-    {({setCurrentContainerName}) => (
-      <div className='mobile'>
-        <MobileWindow isDefault={true} name='home' path='/mobile'>
-          {() => (
-            <HomeScreen onIconClick={name => setCurrentContainerName(name)}/>
-          )}
-        </MobileWindow>
-        {apps.map(app => (
-          <MobileWindow key={app} name={app}>
+  <div className='mobile-example'>
+    <h2>Mobile example</h2>
+    <div className="description">
+      <p>
+        A window group with `allowInterContainerHistory` whose
+        windows are 100% width and height, therefore hiding each other
+      </p>
+    </div>
+    <WindowGroup name='mobile' allowInterContainerHistory={true}>
+      {({setCurrentContainerName}) => (
+        <div className='phone'>
+          <MobileWindow isDefault={true} name='home' path='/mobile'>
             {() => (
-              <div>{app}</div>
+              <HomeScreen onIconClick={name => setCurrentContainerName(name)}/>
             )}
           </MobileWindow>
-        ))}
-      </div>
-    )}
-  </WindowGroup>
+          {apps.map(app => (
+            <MobileWindow key={app} name={app}>
+              {() => (
+                <MobilePage title={app}>
+                  <div>{app}</div>
+                </MobilePage>
+              )}
+            </MobileWindow>
+          ))}
+        </div>
+      )}
+    </WindowGroup>
+  </div>
 )
