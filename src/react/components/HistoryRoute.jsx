8b13886b26f831061bf5458ca0e02296a12c6bae
Ken Fehling <ken@androidideas.org>
2017-03-19 08:13:23 -0400

    Updating HistoryRoute

--- src/react/components/HistoryRoute.jsx
+++ src/react/components/HistoryRoute.jsx
@@ -3,25 +3,27 @@ import matchPath from 'react-router/matchPath'
 import AnimatedPage from './AnimatedPage'
 
 const computeMatch = (pathname, { computedMatch, path, exact, strict }) =>
-computedMatch || matchPath(pathname, path, { exact, strict })
+    computedMatch || matchPath(pathname, path, { exact, strict })
 
-const r = ({component, children, render, match, props}) => (
-  component ? ( // component prop gets first priority, only called if there's a match
-      match ? React.createElement(component, props) : null
-    ) : render ? ( // render prop is next, only called if there's a match
-        match ? render(props) : null
-      ) : children ? ( // children come last, always called
-          typeof children === 'function' ? (
-              children(props)
-            ) : !Array.isArray(children) || children.length ? ( // Preact defaults to empty children array
-                React.Children.only(children)
-              ) : (
-                null
-              )
-        ) : (
-          null
-        )
-)
+const r = ({component, children, render, match, props}) => {
+  return (
+    component ? ( // component prop gets first priority, only called if there's a match
+        match ? React.createElement(component, props) : null
+      ) : render ? ( // render prop is next, only called if there's a match
+          match ? render(props) : null
+        ) : children ? ( // children come last, always called
+            typeof children === 'function' ? (
+                children(props)
+              ) : !Array.isArray(children) || children.length ? ( // Preact defaults to empty children array
+                  React.Children.only(children)
+                ) : (
+                  null
+                )
+          ) : (
+            null
+          )
+  )
+}
 
 class InnerHistoryRoute extends Component {
 
