2fe28ba7421e7b5414f4f5ff99414186dea250fd
Ken Fehling <ken@androidideas.org>
2017-04-16 19:14:45 -0400

    Still not 100% but seems less harmful

--- src/components/NestedGroup.js
+++ src/components/NestedGroup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   Container, ContainerGroup, WindowGroup, HistoryWindow, HistoryRoute,
   HeaderLink, HistoryLink, BackLink
--- src/components/StateTree.js
+++ src/components/StateTree.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connectToStore, waitForInitialization} from 'react-router-nested-history'
 import './StateTree.css'
 
